76b6a5f57aa47d7555ff3a16d8e5e250c4662cc6
dommodnet <102274374+dommodnet@users.noreply.github.com>
2022-09-03 13:39:16 +1200

    Update Esp32-ascii-art.c

--- Esp32-ascii-art.c
+++ Esp32-ascii-art.c
@@ -3,28 +3,41 @@ ESP-32 38Pin
 Having fun with a ESP32 - Add this ESP-32 Ascii Art Pinout into your source code as a reference - Add your own pin definition infomation to it.
 
 
-                                         +-----------------------+
-                                         | O      | USB |      O |
-                                         |        -------        |
-                                     3V3 | [ ]               [ ] | VIN
-                                     GND | [ ]               [ ] | GND
-     Touch3 / HSPI_CS0 / ADC2_3 / GPIO15 | [ ]               [ ] | GPIO13 / ADC2_4 / HSPI_ID / Touch4
- CS / Touch2 / HSPI_WP / ADC2_2 /  GPIO2 | [ ]               [ ] | GPIO12 / ADC2_5 / HSPI_Q / Touch5
-      Touch0 / HSPI_HD / ADC2_0 /  GPIO4 | [ ]               [ ] | GPIO14 / ADC2_6 / HSPI_CLK / Touch6
-                         U2_RXD / GPIO16 | [ ]               [ ] | GPIO27 / ADC2_7 / Touch7
-                         U2_TXD / GPIO17 | [ ]               [ ] | GPIO26 / ADC2_9 / DAC2
-                      V_SPI_CS0 /  GPIO5 | [ ]  ___________  [ ] | GPIO25 / ADC2_8 / DAC1
-                SCK / V_SPI_CLK / GPIO18 | [ ] |           | [ ] | GPIO33 / ADC1_5 / Touch8 / XTAL32
-        U0_CTS / MSIO / V_SPI_Q / GPIO19 | [ ] |           | [ ] | GPIO32 / ADC1_4 / Touch9 / XTAL32
-                 SDA / V_SPI_HD / GPIO21 | [ ] |           | [ ] | GPIO35 / ADC1_7 
-                  CLK2 / U0_RXD /  GPIO3 | [ ] |           | [ ] | GPIO34 / ADC1_6 
-                  CLK3 / U0_TXD /  GPIO1 | [ ] |           | [ ] | GPIO39 / ADC1_3 / SensVN 
-        SCL / U0_RTS / V_SPI_WP / GPIO22 | [ ] |           | [ ] | GPIO36 / ADC1_0 / SensVP 
-                MOSI / V_SPI_WP / GPIO23 | [ ] |___________| [ ] | EN 
-                                         |                       |
-                                         |  |  |  ____  ____  |  |
-                                         |  |  |  |  |  |  |  |  |
-                                         |  |__|__|  |__|  |__|  |
-                                         | O                   O |
-                                         +-----------------------+
+                           +------------------------------+
+                           |    |--|  ____  ____  |       |
+                           |    |  |  |  |  |  |  |       |
+                           |    |  |__|  |__|  |__|       |
+                           |            -------           |
+                           | [ 3V3 ] 1           38   [ ] | GND
+                           | [ EN ]                  [ ] | MOSI / V_SPI_WP / GPIO23
+     ADC1_0 / SensVP       | [ G36 ]              SCL / U0_RTS / V_SPI_WP / GPIO22
+  GPIO39 / ADC1_3 / SensVN | [ G39 ]                   [ ] |        CLK3 / U0_TXD /  GPIO1 
+  | [ G34 ]                  [ ] |     CLK2 / U0_RXD /  GPIO3 
+              ADC1_7        | [ G35 ]                 [ ] |     SDA / V_SPI_HD / GPIO21
+      ADC1_4/Touch9        | [ G32 ]                  [ ] | GND
+      ADC1_5/Touch8        | [ G33 ]                     U0_CTS / MSIO / V_SPI_Q / GPIO19
+           [ ] | GPIO25 / ADC2_8 / DAC1
+               [ ] | GPIO26 / ADC2_9 / DAC2
+                [ ] | GPIO27 / ADC2_7 / Touch7                        U2_TXD / GPIO17
+                                 
+           
+                  V_SPI_CS0 /  GPIO5 | [ ]  ___________  
+                SCK / V_SPI_CLK / GPIO18 | [ ] |           | [ ] | 
+       | [ ] |           | [ ] | GPIO32 / ADC1_4 / Touch9 / XTAL32
+                | [ ] |           | [ ] | 
+                |           | [ ] | GPIO34 / ADC1_6 
+              |           | [ ] | 
+      
+               
+                 
+ ADC2_6 / HSPI_CLK / Touch6 | [ GPIO14]               [ GP16 ]|  U2_RXD / GPIO16 | [ ]
+ ADC2_5 / HSPI_Q / Touch5   | [ GPIO12]               [ GP4 ] |  Touch0 / HSPI_HD / ADC2_0 /  
+                            | [ GND ]                [ GPI0 ] |  Touch1 / HSPI_WP / ADC2_1 
+     ADC2_4/HSPI_ID/Touch4  | [ GP13 ]               [ GPI2 ] |  Touch2 / HSPI_WP / ADC2_2 
+                            | [ GP9 ]                [ GP15 ] |  Touch3/ ADC2_3 
+                            | [ GP10 ]                [ GP9 ] |                           
+                            | [ GP11 ]                [ GP7 ] |   
+                            | [ VIN 5V ]              [ GP6 ] |
+                            | O           | USB |           O |
+                            +---------------------------------+
 */
